279daf5f2cbb487224e1cd4de317453a528b62ca
Nico Gräf <graef.nico@gmail.com>
2020-02-10 02:38:56 +0100

    Updates

--- src/components/Avatar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import me from '../images/me.jpg'
-
-export default ({className}) => (
-  <img src={me} alt="Nico Graef" className={className} style={{ maxWidth: 250, borderRadius: '50%' }} />
-)
--- src/components/Chip.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default ({ text }) => (
-  <span style={{ background: '#dbe2ef', padding: 3, borderRadius: 3, marginRight: 6 }}>
-    <small>{text}</small>
-  </span>
-)
