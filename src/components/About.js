709b624871c190bb7b785611b63bb0ed6f2c8033
Nico Gräf <graef.nico@gmail.com>
2020-02-10 03:17:46 +0100

    changed about text

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,16 @@ export default () => (
     <div className="section-content">
       <p>
         Since I got into programming 10 years ago I worked for many different projects for fun and
-        professionally. While studying computer science and micro electronics I earned my living as
-        a freelance software developer, web designer and online marketing consultant. In 2017 I
+        professionally. While studying computer science and electronics I earned my living as a
+        freelance software developer, web designer and online marketing consultant. In 2017 I
         founded a startup trying to build a platform for community-driven projects. During this time
         I developed a native android app, a hybrid app and several features for the web backend. I'm
         currently focusing on Progressive Web Apps built with React JS.
       </p>
 
       <p>
-        To free my mind from callbacks and linter warnings I sometimes cook with friends, listen to
-        a podcast while drinking coffee or go for a run at the Dreisam.
+        To free my mind from callbacks and linter warnings I sometimes cook together with friends,
+        listen to a podcast while drinking coffee or go for a run at the Dreisam.
       </p>
     </div>
   </section>
